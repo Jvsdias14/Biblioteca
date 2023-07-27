4750e91dfa34668e4f1c2bfebdbbdac7ee74d090
Jvsdias14 <jvsdias2012@gmail.com>
2023-07-27 08:25:04 -0300

    Algo na tela versão 1.1

--- classescobrinha.py
+++ classescobrinha.py
@@ -1,9 +1,11 @@
 class Cobra():
     def __init__(self):
         self.cor = "green"
-        self.altura = "0.05"
-        self.comprimento = "0.05"
-        self.func()
+        self.altura = 0.05
+        self.comprimento = 0.05
+        #self.func()
+    
+    
     def func(self):
         print('teste')
     
--- cobrinha.py
+++ cobrinha.py
@@ -1,3 +1,23 @@
-from cobrinhajg.classescobrinha import Cobra
+from classescobrinha import Cobra
+import pygame
 
-Cobra()
+class Application():
+    def __init__(self):
+        pygame.init()
+        self.tela = pygame.display.set_mode((600,450))
+        self.player_pos = pygame.Vector2(self.tela.get_width() / 2, self.tela.get_height() / 2)
+        self.crobation()
+        pygame.display.flip()
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    
+    def crobation(self):
+        pygame.draw.circle(self.tela, "midnightblue", self.player_pos , 80)
+        
+        
+
+Application()
