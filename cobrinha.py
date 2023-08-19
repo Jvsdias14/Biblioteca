d14ada33f3c1cf7db453d03cf2778b7519ce3d75
Jvsdias14 <jvsdias2012@gmail.com>
2023-08-19 00:13:24 -0300

    croba 1.0

--- cobrinha.py
+++ cobrinha.py
@@ -1,5 +1,8 @@
 from classescobrinha import Cobra
 import pygame
+import sys
+from time import sleep
+import random
 
 class Application():
     def __init__(self):
@@ -9,15 +12,51 @@ class Application():
         self.crobation()
         pygame.display.flip()
         running = True
-
+        self.command = ""
+        self.cria_maca()  
+         
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.command = "UP" 
+                    if event.key == pygame.K_DOWN:      
+                        self.command = "DOWN"
+                    if event.key == pygame.K_LEFT:
+                        self.command = "LEFT"
+                    if event.key == pygame.K_RIGHT:
+                        self.command = "RIGHT"
+                        
+            if self.command == "UP":
+                self.player_pos[1] -= 35
+            if self.command == "DOWN":
+                self.player_pos[1] += 35
+            if self.command == "LEFT":
+                self.player_pos[0] -= 35
+            if self.command == "RIGHT":
+                self.player_pos[0] += 35
+                          
+            self.crobation() 
+            self.pega_maca()      
+   
+    def crobation(self):   
+        sleep(0.2)
+        self.tela.fill("black")
+        pygame.draw.rect(self.tela, "green", [self.player_pos[0] - 25,self.player_pos[1] -25,50,50],0,7)
+        pygame.display.update()
+        
+    def pega_maca(self):
+        pygame.draw.rect(self.tela, "red", [0 + self.posx, 0 + self.posy,25,25],0,7)
+        pygame.display.update()
+        
+    def cria_maca(self):
+        sorte1 = random.randint(1,9)
+        sorte2 = random.randint(1,9)
+        
+        self.posx = 50 * sorte1
+        self.posy = 50 * sorte2
                     
-    def crobation(self):
-        pygame.draw.circle(self.tela, "midnightblue", self.player_pos , 80)
-        
-        
-
+            
 Application()
